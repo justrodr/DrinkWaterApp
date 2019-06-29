f0303beafa850a57ac22afdf31e4022993f7f518
dh2487 <davidhdoev@gmail.com>
2019-06-29 11:41:30 -0700

    Added firebase to the app and pods

--- DrinkWater/AppDelegate.swift
+++ DrinkWater/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         // Overriding the didFinishLaunching to get rid of storyboard and use homeViewController
-        
+        FirebaseApp.configure()
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'DrinkWater' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+
+  # Pods for DrinkWater
+
+  target 'DrinkWaterTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'DrinkWaterUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
